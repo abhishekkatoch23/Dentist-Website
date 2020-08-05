0be47e9b9d7bd5027e8f93ffebf66340521e5684
abhishek katoch <abhishekkatoch434@gmail.com>
2020-08-05 22:22:57 +0530

    little change

--- dentist/settings.py
+++ dentist/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8ugn2be@pu2fgaq3f3hst!h*g(9v02f@!r*ws7@%47l174$dw_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'pacific-beach-29903.herokuapp.com']
 
 
 # Application definition
